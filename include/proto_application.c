8e46ca8e6069802c910cf8b5ecee38ad8bc1e1af
DanDan <1187480520@qq.com>
2023-04-20 12:23:01 -0400

    添加报文结构链表

--- include/proto_application.c
+++ include/proto_application.c
@@ -24,29 +24,15 @@ u_int32_t analysis_http(void* pkt_ptr, void* app_buffer,  uint32_t tcp_len,  voi
 u_int32_t analysis_ssh(void* pkt_ptr, void* app_buffer,  uint32_t tcp_len,  void* res_ptr)
 {
     /* TODO: 如何判断一个数据是不是ssh
-        1、SSH一般默认端口是22,并且应该是长度大于7(不一定准,测试长度大于7得出的结果和wirshark一样)
-        2、一般开始链接的时候,有数据头有SSH-2.0版本的信息,可以比较字符串SSH-
+        1、SSH一般默认端口是22,并且应为要加密,长度应该是长度大于10(不一定准,测试长度大于7得出的结果和wirshark一样)
+        2、一般开始链接的时候,有数据头有SSH-2.0版本的信息,可以比较字符串SSH-,并且建立连接后,之后这两个IP的这两个端口通讯一定是ssh
         3、数据段中有SSH-字段
     */
-    if( 7> tcp_len || NULL == app_buffer)
+    if( NULL == app_buffer)
         return 0;
 
-    // 指向tcp头的指针是不是为空
-    if(NULL != ((dpi_pkt*)pkt_ptr)->tcp_head_ptr  || NULL != pkt_ptr )
-    {
-        // tcp头指针不为空,判断通信双方端口有没有22
-        u_int32_t port = ntohs(22);
-        if( port == ((dpi_pkt*)pkt_ptr)->tcp_head_ptr->tcp_sport || port == ((dpi_pkt*)pkt_ptr)->tcp_head_ptr->tcp_dport)
-        {
-            ((dpi_pkt*)pkt_ptr)->ssh_head_ptr = app_buffer;
-            ((dpi_pkt*)pkt_ptr)->ssh_len = tcp_len;
-            if(NULL != res_ptr)
-                ++ ((dpi_result*)res_ptr)->tcp_proto_count[SSH];
-            return 1;
-        }
-    }
-
-    // 判断数据头有没有 SSH- 字样
+        // 判断数据头有没有 SSH- 字样,
+        // 如果有说明他在建立SSH连接,将他添加到他IP和端口记录到链表中,之后所有的这个IP和端口的通讯都是SSH
     if(0 == strcmp("SSH-", app_buffer))
     {
         if(NULL != ((dpi_pkt*)pkt_ptr)->tcp_head_ptr  || NULL != pkt_ptr )
@@ -58,6 +44,28 @@ u_int32_t analysis_ssh(void* pkt_ptr, void* app_buffer,  uint32_t tcp_len,  void
             ++ ((dpi_result*)res_ptr)->tcp_proto_count[SSH];
         return 1;
     }
+
+    // 指向tcp头的指针是不是为空,并且字段大于10
+    if(NULL != ((dpi_pkt*)pkt_ptr)->tcp_head_ptr  && NULL != pkt_ptr  && 10 < tcp_len )
+    {
+        
+        u_int32_t port = ntohs(22);
+
+        // 遍历已知的建立的ssh的链表中他的IP
+        
+
+        // 判断通信双方端口有没有22+通过长度判断
+        if( port == ((dpi_pkt*)pkt_ptr)->tcp_head_ptr->tcp_sport || port == ((dpi_pkt*)pkt_ptr)->tcp_head_ptr->tcp_dport)
+        {
+            ((dpi_pkt*)pkt_ptr)->ssh_head_ptr = app_buffer;
+            ((dpi_pkt*)pkt_ptr)->ssh_len = tcp_len;
+            if(NULL != res_ptr)
+                ++ ((dpi_result*)res_ptr)->tcp_proto_count[SSH];
+            return 1;
+        }
+    }
+
+
     return 0;
 }
 
