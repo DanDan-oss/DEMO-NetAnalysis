f8bc7075d02a998e0f20d448590eb2a831b25c36
dandan <81583135+DanDan-oss@users.noreply.github.com>
2023-04-22 23:58:57 +0800

    优化链表结构,并调试链表 (#15)

--- utils/dpi_list.h
+++ utils/dpi_list.h
@@ -4,19 +4,18 @@
 #include <netinet/in.h>
 
 // 链表节点结构
-typedef struct _proto_list_node
+typedef struct _proto_node
 {
     void* data;
-    struct _proto_list_node* Back;
-    struct _proto_list_node* Next;
-}proto_list_node_t, *proto_list_node_ptr;
-
+    struct _proto_node* Back;
+    struct _proto_node* Next;
+}proto_node_t, *proto_node_ptr;
 
 // 链表
 typedef struct _proto_list
 {
     uint32_t node_count;
-    proto_list_node_t* head;
+    proto_node_t* head;
 }proto_list_t, *proto_list_ptr;
 
 // 创建一个新节点
@@ -29,5 +28,11 @@ int proto_list_addNode(proto_list_t* list, void* data);
 
 // 移除某一个节点
 int proto_list_delNode(proto_list_t* list, void* data);
+
+// 遍历链表打印链表节点中的值
+void ProtoListPrint(proto_list_t* list);
+
+// 测试链表结构
+void ProtoDubgPrint();
 
 #endif
